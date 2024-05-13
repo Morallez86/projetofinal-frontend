8bc5a9c25f46f09b68b50f27deea913580e2f9fb
Joao Morais <joaomorais@student.dei.uc.pt>
2024-05-13 22:05:45 +0100

    tailwind

---
+++ projetofinal-frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from '@material-tailwind/react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Button>
+        Click Me
+      </Button>
+    </div>
+  );
+}
+
+export default App;
--- projetofinal-frontend/src/index.js
+++ projetofinal-frontend/src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from '@material-tailwind/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+      <ThemeProvider>
+        <Routes>
+          <Route index element={<App />} />
+        </Routes>
+      </ThemeProvider>
+    </Router>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
