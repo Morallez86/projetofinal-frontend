077bf4f91aaf612553a456edac2355df685c2257
ricardoelias98 <uc2016225298@student.uc.pt>
2024-05-18 16:44:32 +0100

    alerts do login conectados

--- projetofinal-frontend/src/components/LoginCard.jsx
+++ projetofinal-frontend/src/components/LoginCard.jsx
@@ -15,29 +15,34 @@ function LoginCard() {
     setFormData((prevData) => ({ ...prevData, [name]: value }));
   };
 
+  const [warning, setWarning] = useState(0);
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    fetch ("http://localhost:8080/projetofinal-backend-1.0-SNAPSHOT/rest/users/login", {
-      method: "POST",
-      headers: {
-        Accept: "*/*",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    })
+    fetch(
+      "http://localhost:8080/projetofinal-backend-1.0-SNAPSHOT/rest/users/login",
+      {
+        method: "POST",
+        headers: {
+          Accept: "*/*",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      }
+    )
       .then(async function (response) {
         if (response.status === 401) {
-          console.log("Invalid information")
+          setWarning(1);
+          console.log("Invalid information");
         } else if (response.status === 200) {
-          console.log("Sucessfull login")
+          setWarning(0);
+          console.log("Sucessfull login");
         }
       })
       .catch((error) => {
         console.error("Error fetching user:", error);
       });
-  }
-
-
+  };
 
   return (
     <Card className="max-w-sm">
@@ -71,16 +76,24 @@ function LoginCard() {
         <Button type="submit">Submit</Button>
         <Button icon={RiLoginCircleFill}>Forgot password?</Button>
       </form>
-      <Alert color="failure" icon={HiInformationCircle}>
-        <span className="font-medium"> </span> Incorrect information! Try again
-      </Alert>
-      <Alert color="warning" icon={HiInformationCircle} rounded>
-        <span className="font-medium" style={{ textDecoration: "underline" }}>
-          {" "}
-          REMEMBER!{" "}
-        </span>{" "}
-        Validate your account in your email if you haven't already done so
-      </Alert>
+      {warning === 1 && (
+        <>
+          <Alert color="failure" icon={HiInformationCircle}>
+            <span className="font-medium">
+              Incorrect information! Try again.
+            </span>
+          </Alert>
+          <Alert color="warning" icon={HiInformationCircle} rounded>
+            <span
+              className="font-medium"
+              style={{ textDecoration: "underline" }}
+            >
+              REMEMBER!
+            </span>{" "}
+            Validate your account in your email if you haven't already done so.
+          </Alert>
+        </>
+      )}
     </Card>
   );
 }
