6abd03d67fa3842c6a3d6f63bb1c4374713418df
ricardoelias98 <uc2016225298@student.uc.pt>
2024-05-21 12:41:19 +0100

    inicio da homePage

--- projetofinal-frontend/src/index.js
+++ projetofinal-frontend/src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainPage from "./Pages/MainPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -12,6 +13,7 @@ root.render(
     <Router>
       <Routes>
         <Route index element={<App />} />
+        <Route path="/home" element={<MainPage />} />
       </Routes>
     </Router>
   </React.StrictMode>
