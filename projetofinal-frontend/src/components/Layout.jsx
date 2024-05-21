fb0af2ca2c2256bd72e7c733f6e81490d4865ba7
ricardoelias98 <uc2016225298@student.uc.pt>
2024-05-21 16:48:28 +0100

    Avanços no layout
    tabs com espaços certos e formatos

--- projetofinal-frontend/src/components/Layout.jsx
+++ projetofinal-frontend/src/components/Layout.jsx
@@ -1,53 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import { ImProfile } from "react-icons/im";
 import { AiOutlineFundProjectionScreen } from "react-icons/ai";
 import { GrResources } from "react-icons/gr";
 import { Tabs } from "flowbite-react";
 
-function Layout({activeTab}) {
+function Layout({ activeTab }) {
   return (
     <div className="flex flex-col items-center">
-      <Tabs aria-label="Default tabs" className="w-full max-w-xl">
+      <Tabs
+        aria-label="Full width tabs"
+        style="fullWidth"
+        defaultValue={activeTab}
+      >
+        <Tabs.Item value={0} title="MyProfile" icon={ImProfile}></Tabs.Item>
         <Tabs.Item
-          active={activeTab === 0}
-          title="MyProfile"
-          icon={ImProfile}
-        ></Tabs.Item>
-        <Tabs.Item
-          active={activeTab === 1}
+          value={1}
           title="All Projects"
           icon={AiOutlineFundProjectionScreen}
         ></Tabs.Item>
         <Tabs.Item
-          active={activeTab === 2}
+          value={2}
           title="Components/Resources"
           icon={GrResources}
         ></Tabs.Item>
       </Tabs>
 
-      <div className="mt-4 w-full max-w-xl">
+      <div className="w-full max-w-xl">
         {activeTab === 0 && (
-          <Tabs aria-label="MyProfile sub-tabs" className="w-full">
-            {console.log("Rendering MyProfile sub-tabs")}
-            <Tabs.Item title="My Projects"></Tabs.Item>
-            <Tabs.Item title="Edit Profile"></Tabs.Item>
-            <Tabs.Item title="Register Skill/Interest"></Tabs.Item>
-            <Tabs.Item title="Messages"></Tabs.Item>
-          </Tabs>
+          <div>
+            <Tabs aria-label="Pills" style="pills" className="w-full">
+              {console.log("Rendering MyProfile sub-tabs")}
+              <Tabs.Item
+                title={<span className="text-black">My Projects</span>}
+              ></Tabs.Item>
+              <Tabs.Item
+                title={<span className="text-black">Edit Profile</span>}
+              ></Tabs.Item>
+              <Tabs.Item
+                title={
+                  <span className="text-black">Register Skill/Interest</span>
+                }
+              ></Tabs.Item>
+              <Tabs.Item
+                title={<span className="text-black">Messages</span>}
+              ></Tabs.Item>
+            </Tabs>
+          </div>
         )}
         {activeTab === 1 && (
-          <Tabs aria-label="All Projects sub-tabs" className="w-full">
-            {console.log("Rendering All Projects sub-tabs")}
-            <Tabs.Item title="Create New"></Tabs.Item>
-            <Tabs.Item title="Projects List"></Tabs.Item>
-          </Tabs>
+          <div>
+            <Tabs aria-label="Pills" style="pills" className="w-full">
+              {console.log("Rendering All Projects sub-tabs")}
+              <Tabs.Item
+                title={<span className="text-black">Create New</span>}
+              ></Tabs.Item>
+              <Tabs.Item
+                title={<span className="text-black">Projects List</span>}
+              ></Tabs.Item>
+            </Tabs>
+          </div>
         )}
         {activeTab === 2 && (
-          <Tabs aria-label="Components/Resources sub-tabs" className="w-full">
-            {console.log("Rendering Components/Resources sub-tabs")}
-            <Tabs.Item title="Components"></Tabs.Item>
-            <Tabs.Item title="Resources"></Tabs.Item>
-          </Tabs>
+          <div>
+            <Tabs aria-label="Pills" style="pills" className="w-full ">
+              {console.log("Rendering Components/Resources sub-tabs")}
+              <Tabs.Item
+                title={<span className="text-black">Components</span>}
+              ></Tabs.Item>
+              <Tabs.Item
+                title={<span className="text-black">Resources</span>}
+              ></Tabs.Item>
+            </Tabs>
+          </div>
         )}
       </div>
     </div>
