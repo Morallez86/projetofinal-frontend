373e28c6c932f68b4146740ae28673f5327c7c5b
ricardoelias98 <uc2016225298@student.uc.pt>
2024-05-22 01:55:12 +0100

    avanços na página de perfil

--- projetofinal-frontend/src/Pages/MyProfile_AboutMe.jsx
+++ projetofinal-frontend/src/Pages/MyProfile_AboutMe.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
-import Layout from '../Components/Layout';
+import React from "react";
+import Layout from "../Components/Layout";
+import { Avatar } from "flowbite-react";
+import ProfileCard from "../Components/ProfileCard";
 
 function MyProfile_AboutMe() {
-    return <Layout activeTab={0} activeSubTabProfile={4} />;
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Layout activeTab={0} activeSubTabProfile={4} />
+      <div className="flex-grow flex p-4">
+        <div className="justify-center w-1/3">
+          <ProfileCard />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default MyProfile_AboutMe
+export default MyProfile_AboutMe;
