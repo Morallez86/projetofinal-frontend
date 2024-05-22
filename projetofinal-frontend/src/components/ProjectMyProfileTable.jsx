69923881e855f10fbbba84d6bd8c568eab095902
ricardoelias98 <uc2016225298@student.uc.pt>
2024-05-22 12:20:10 +0100

    tabela filtravel, ordenavel e paginada

--- projetofinal-frontend/src/components/ProjectMyProfileTable.jsx
+++ projetofinal-frontend/src/components/ProjectMyProfileTable.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import DataTable from "react-data-table-component";
+import { useState } from "react";
 
 function ProjectMyProfileTable() {
   const columns = [
@@ -21,12 +22,46 @@ function ProjectMyProfileTable() {
     },
   ];
 
+  const data = [
+    {
+      id: 1,
+      title: "Project 1",
+      creation_date: "08/04/2024",
+      status: "Pending",
+    },
+    {
+      id: 2,
+      title: "Project 2",
+      creation_date: "10/08/2023",
+      status: "Finished",
+    },
+    {
+      id: 3,
+      title: "Project 3",
+      creation_date: "10/08/2022",
+      status: "Finished",
+    },
+  ];
+
+  const [records, setRecords] = useState(data);
+
+  function handleFilter(e){
+    const value = e.target.value;
+    const filteredData = data.filter((record) => {
+      return record.title.toLowerCase().includes(value.toLowerCase());
+    });
+    setRecords(filteredData);
+  }
+
   return (
     <div>
+        <div>
+            <input type="text" onChange={handleFilter}/>
+        </div>
       <DataTable
         title="My Projects"
         columns={columns}
-        data={[]}
+        data={records}
         fixedHeader
         pagination
         responsive
