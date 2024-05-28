0dc41f44f34c7f7a117914a2c7efbfe6086bef61
Joao Morais <joaomorais@student.dei.uc.pt>
2024-05-29 00:28:47 +0100

    put para profile. Campos editáveis.

--- projetofinal-frontend/src/components/ProfileCard.jsx
+++ projetofinal-frontend/src/components/ProfileCard.jsx
@@ -1,11 +1,11 @@
-import { Card } from "flowbite-react";
-import { Avatar } from "flowbite-react";
+import { Card, TextInput, Label, Button, Textarea, Avatar } from "flowbite-react";
 import useUserStore from "../Stores/UserStore";
 import { useEffect, useState } from "react";
 import { jwtDecode } from "jwt-decode";
 import { LuPlusCircle } from "react-icons/lu";
 import { Tooltip } from "react-tooltip";
 import { MdOutlineRemoveCircleOutline } from "react-icons/md";
+import { MdOutlineEdit } from "react-icons/md";
 
 function ProfileCard({ openPopUpSkills, openPopUpInterests, openPopUpSkillsRemove }) {
   const token = useUserStore((state) => state.token);
@@ -14,10 +14,7 @@ function ProfileCard({ openPopUpSkills, openPopUpInterests, openPopUpSkillsRemov
   const setSkills = useUserStore((state) => state.setSkills);
   const setInterests = useUserStore((state) => state.setInterests);
 
-  useEffect(() => {
-    fetchUserInfo();
-  }, []);
-
+  const [editMode, setEditMode] = useState(false);
   const [userInfo, setUserInfo] = useState({
     name: "",
     jobLocation: "",
@@ -26,6 +23,10 @@ function ProfileCard({ openPopUpSkills, openPopUpInterests, openPopUpSkillsRemov
     interests: [],
     biography: "",
   });
+
+  useEffect(() => {
+    fetchUserInfo();
+  }, []);
 
   const fetchUserInfo = async () => {
     try {
@@ -62,44 +63,134 @@ function ProfileCard({ openPopUpSkills, openPopUpInterests, openPopUpSkillsRemov
     }
   };
 
+  const handleEditClick = () => {
+    setEditMode(true);
+  };
+
+  const handleSaveClick = async () => {
+    // Save the updated user info here
+    try {
+      const response = await fetch(
+        `http://localhost:8080/projetofinal-backend-1.0-SNAPSHOT/rest/users/profile/${userId}`,
+        {
+          method: "PUT",
+          headers: {
+            Accept: "*/*",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify({
+            firstName: userInfo.name.split(' ')[0],
+            lastName: userInfo.name.split(' ')[1],
+            username: userInfo.nickname,
+            biography: userInfo.biography,
+            workplace: userInfo.jobLocation,
+          }),
+        }
+      );
+
+      if (response.status === 200) {
+        console.log("User info updated successfully");
+        setEditMode(false);
+      } else {
+        console.error("Error updating user info");
+      }
+    } catch (error) {
+      console.error("Error updating user info:", error);
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserInfo((prevInfo) => ({
+      ...prevInfo,
+      [name]: value,
+    }));
+  };
+
   return (
     <Card className="max-w-sm bg-transparent hover:bg-gray-200 transition-colors duration-200">
-      <div className="flex flex-col pb-10 ">
-        <Avatar
-          img="https://byuc.wordpress.com/wp-content/uploads/2012/07/avat-2.jpg?w=640"
-          alt="avatar"
-          size={"xl"}
-          rounded
-        />
-        <div className="mt-4">
-          <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-            Name
-          </h5>
-          <p className="text-sm text-gray-500 dark:text-gray-400">
-            {userInfo.name}
-          </p>
+      <div className="flex flex-col pb-10">
+        <div className="relative flex items-center justify-center">
+          <Avatar
+            img="https://byuc.wordpress.com/wp-content/uploads/2012/07/avat-2.jpg?w=640"
+            alt="avatar"
+            size="xl"
+            rounded
+          />
+          <MdOutlineEdit
+            className="h-6 w-6 text-black cursor-pointer absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2"
+            onClick={handleEditClick}
+          />
         </div>
         <div className="mt-4">
-          <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-            Job Location
-          </h5>
-          <p className="text-sm text-gray-500 dark:text-gray-400">
-            {userInfo.jobLocation}
-          </p>
+          <Label htmlFor="name" value="Name" />
+          {editMode ? (
+            <TextInput
+              id="name"
+              type="text"
+              name="name"
+              value={userInfo.name}
+              onChange={handleChange}
+              className="text-sm text-gray-500 dark:text-gray-400"
+            />
+          ) : (
+            <p className="text-sm text-gray-500 dark:text-gray-400">{userInfo.name}</p>
+          )}
         </div>
         <div className="mt-4">
-          <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-            Nickname
-          </h5>
-          <p className="text-sm text-gray-500 dark:text-gray-400">
-            {userInfo.nickname}
-          </p>
+          <Label htmlFor="jobLocation" value="Job Location" />
+          {editMode ? (
+            <TextInput
+              id="jobLocation"
+              type="text"
+              name="jobLocation"
+              value={userInfo.jobLocation}
+              onChange={handleChange}
+              className="text-sm text-gray-500 dark:text-gray-400"
+            />
+          ) : (
+            <p className="text-sm text-gray-500 dark:text-gray-400">{userInfo.jobLocation}</p>
+          )}
         </div>
+        <div className="mt-4">
+          <Label htmlFor="nickname" value="Nickname" />
+          {editMode ? (
+            <TextInput
+              id="nickname"
+              type="text"
+              name="nickname"
+              value={userInfo.nickname}
+              onChange={handleChange}
+              className="text-sm text-gray-500 dark:text-gray-400"
+            />
+          ) : (
+            <p className="text-sm text-gray-500 dark:text-gray-400">{userInfo.nickname}</p>
+          )}
+        </div>
+        <div className="mt-4">
+          <Label htmlFor="biography" value="Biography" />
+          {editMode ? (
+            <Textarea
+              id="biography"
+              name="biography"
+              value={userInfo.biography}
+              onChange={handleChange}
+              className="text-sm text-gray-500 dark:text-gray-400"
+              rows={2}
+            />
+          ) : (
+            <p className="text-sm text-gray-500 dark:text-gray-400">{userInfo.biography}</p>
+          )}
+        </div>
+        {editMode && (
+          <Button onClick={handleSaveClick} className="mt-4">
+            Save
+          </Button>
+        )}
         <div className="mt-4">
           <div className="flex items-center">
-            <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-              Skills
-            </h5>
+            <Label htmlFor="skills" value="Skills" />
             <div
               id="icon-element"
               className="inline-flex items-center cursor-pointer"
@@ -148,9 +239,7 @@ function ProfileCard({ openPopUpSkills, openPopUpInterests, openPopUpSkillsRemov
         </div>
         <div className="mt-4">
           <div className="flex items-center">
-            <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-              Interests
-            </h5>
+            <Label htmlFor="interests" value="Interests" />
             <div
               id="icon-element-interests"
               className="inline-flex items-center cursor-pointer"
@@ -184,14 +273,6 @@ function ProfileCard({ openPopUpSkills, openPopUpInterests, openPopUpSkillsRemov
                 </div>
               )}
           </div>
-        </div>
-        <div className="mt-4">
-          <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-            Biography
-          </h5>
-          <p className="text-sm text-gray-500 dark:text-gray-400">
-            {userInfo.biography}
-          </p>
         </div>
       </div>
     </Card>
