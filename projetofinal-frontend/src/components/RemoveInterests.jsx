7441d42975aea319bf1ebee3da4972ee5dd3e528
ricardoelias98 <uc2016225298@student.uc.pt>
2024-05-30 15:15:48 +0100

    org do modal do remove interesses

--- projetofinal-frontend/src/components/RemoveInterests.jsx
+++ projetofinal-frontend/src/components/RemoveInterests.jsx
@@ -2,7 +2,10 @@ import React, { useState } from "react";
 import { Modal, Button, TextInput } from "flowbite-react";
 import useUserStore from "../Stores/UserStore";
 import { Checkbox, Label } from "flowbite-react";
-import useApiStore from '../Stores/ApiStore';
+import useApiStore from "../Stores/ApiStore";
+import RemovedAnimation from "../Assets/Removed.json";
+import Lottie from "react-lottie";
+import { Tooltip } from "react-tooltip";
 
 function RemoveInterests({
   openPopUpInterestRemove,
@@ -16,6 +19,7 @@ function RemoveInterests({
 
   const [filter, setFilter] = useState("");
   const [selectedInterestIds, setSelectedInterestIds] = useState([]);
+  const [animationPlayed, setAnimationPlayed] = useState(false);
 
   const filteredInterest = userInterests.filter((interest) =>
     interest.name.toLowerCase().includes(filter.toLowerCase())
@@ -34,31 +38,37 @@ function RemoveInterests({
   const handleRemoveInterests = async () => {
     console.log(selectedInterestIds);
     try {
-      const response = await fetch(
-        `${apiUrl}/interests`,
-        {
-          method: "DELETE",
-          headers: {
-            Accept: "*/*",
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-          body: JSON.stringify(selectedInterestIds),
-        }
-      );
+      const response = await fetch(`${apiUrl}/interests`, {
+        method: "DELETE",
+        headers: {
+          Accept: "*/*",
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(selectedInterestIds),
+      });
       if (response.status === 204) {
         console.log();
         const updatedInterest = userInterests.filter(
           (interest) => !selectedInterestIds.includes(interest.id)
         );
         setInterests(updatedInterest);
-        closePopUpInterestRemove();
+        setAnimationPlayed(true);
       } else if (response.status === 500) {
         console.log("Internet server error");
       }
     } catch (error) {
       console.error("Error deleting interests:", error);
     }
+  };
+
+  const defaultOptions = {
+    loop: false,
+    autoplay: false,
+    animationData: RemovedAnimation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
   };
 
   return (
@@ -74,28 +84,22 @@ function RemoveInterests({
       >
         <Modal.Header />
         <Modal.Body>
-          <div className="text-center">
-            <h3 className="mb-5 text-lg font-bold text-gray-500 dark:text-gray-400">
+          <div className="flex flex-col items-center justify-center space-y-5">
+            <h3 className="text-lg font-bold text-gray-500 dark:text-gray-400">
               Remove Interest
             </h3>
             <div className="grid grid-cols-2 gap-4">
-              <div>
-                <h4 className="mb-3">
-                  You can remove one or more interests at the same time{" "}
-                </h4>
+              <div className="space-y-3">
+                <h4>You can remove one or more interests at the same time</h4>
                 <TextInput
                   type="text"
                   placeholder="Filter interests"
                   value={filter}
                   onChange={(e) => setFilter(e.target.value)}
-                  className="mb-3"
                 />
-                <div className="flex flex-col items-start overflow-y-auto h-36">
+                <div className="flex flex-col items-start overflow-y-auto h-36 space-y-2">
                   {filteredInterest.map((interest) => (
-                    <div
-                      key={interest.id}
-                      className="flex items-center gap-2 mb-2"
-                    >
+                    <div key={interest.id} className="flex items-center gap-2">
                       <Checkbox
                         id={interest.id ? interest.id.toString() : ""}
                         checked={selectedInterestIds.includes(interest.id)}
@@ -109,11 +113,32 @@ function RemoveInterests({
                     </div>
                   ))}
                 </div>
-                <div className="col-span-full mt-3">
-                  <Button onClick={handleRemoveInterests}>
-                    Remove selected interests
-                  </Button>
-                </div>
+                <Button onClick={handleRemoveInterests}>
+                  Remove selected interests
+                </Button>
+              </div>
+              <div
+                id="icon-element"
+                className="pointer-events-none flex items-center justify-center h-full"
+              >
+                <Lottie
+                  options={defaultOptions}
+                  height={200}
+                  width={200}
+                  isStopped={!animationPlayed}
+                  isPaused={!animationPlayed}
+                  eventListeners={[
+                    {
+                      eventName: "complete",
+                      callback: () => setAnimationPlayed(false),
+                    },
+                  ]}
+                />
+                <Tooltip
+                  anchorSelect="#icon-element"
+                  content="Click to delete this interest from your profile"
+                  place="top"
+                />
               </div>
             </div>
           </div>
