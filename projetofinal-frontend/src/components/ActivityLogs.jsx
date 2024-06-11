1ef827f770adec259bcb8af05247302f516fff02
ricardoelias98 <uc2016225298@student.uc.pt>
2024-06-11 17:23:32 +0100

    falta filtrar e trazer ordenado

--- projetofinal-frontend/src/components/ActivityLogs.jsx
+++ projetofinal-frontend/src/components/ActivityLogs.jsx
@@ -6,6 +6,8 @@ import { useState } from "react";
 function ActivityLogs({ tasks, projectId, logs }) {
   const [showModal, setShowModal] = useState(false);
   const [totalLogs, setTotalLogs] = useState(logs);
+  const [isExpanded, setIsExpanded] = useState(false);
+
 
   const handleOpenModal = () => {
     setShowModal(true);
@@ -27,29 +29,36 @@ function ActivityLogs({ tasks, projectId, logs }) {
     <div className="flex flex-col items-center p-4 space-y-4 rounded-md bg-white">
       <h1 className="text-4xl font-bold underline mb-4">Activity Log</h1>
       <div className="overflow-auto space-y-4 h-[30rem]">
-      {totalLogs.map((log) => {
-  const date = new Date(...log.timestamp);
-  const formattedDate = date.toLocaleDateString();
-  const formattedTime = date.toLocaleTimeString().slice(0, 5);
-  const shortDescription = log.newDescription
-    ? log.newDescription.length > 15
-      ? log.newDescription.slice(0, 15) + " (...)"
-      : log.newDescription
-    : "";
-  const displayText = log.title ? log.title : shortDescription;
+        {totalLogs.map((log) => {
+          const date = new Date(...log.timestamp);
+          const formattedDate = date.toLocaleDateString();
+          const formattedTime = date.toLocaleTimeString().slice(0, 5);
+          const shortDescription = log.newDescription
+            ? log.newDescription.length > 15
+              ? log.newDescription.slice(0, 15) + " (...)"
+              : log.newDescription
+            : "";
+          const displayText = log.title ? log.title : shortDescription;
 
-  return (
-    <div
-      key={log.id}
-      className={`w-72 h-24 rounded-md ${logColors[log.type]}`}
-    >
-      <p className="text-center font-bold">{displayText}</p>
-      <p className="text-center">{formattedDate}</p>
-      <p className="text-center">{formattedTime}</p>
-      <p className="text-center font-bold">{log.userName}</p>
-    </div>
-  );
-})}
+          return (
+            <div
+              key={log.id}
+              className={`w-72 rounded-md border border-black ${
+                logColors[log.type]
+              } ${isExpanded ? "h-auto" : "h-24"}`}
+              onMouseEnter={() => !log.title && setIsExpanded(true)}
+              onMouseLeave={() => !log.title && setIsExpanded(false)}
+            >
+              <p className="text-center font-bold">{displayText}</p>
+              {isExpanded && (
+                <p className="text-center">{log.newDescription}</p>
+              )}
+              <p className="text-center">{formattedDate}</p>
+              <p className="text-center">{formattedTime}</p>
+              <p className="text-center font-bold">{log.userName}</p>
+            </div>
+          );
+        })}
       </div>
       <Button onClick={handleOpenModal} className="mb-3">
         Create log
