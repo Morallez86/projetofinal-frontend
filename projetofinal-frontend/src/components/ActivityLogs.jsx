b43caa736dcfa9423828c38c161a555c544bdd1f
ricardoelias98 <uc2016225298@student.uc.pt>
2024-06-11 15:44:11 +0100

    avanços

--- projetofinal-frontend/src/components/ActivityLogs.jsx
+++ projetofinal-frontend/src/components/ActivityLogs.jsx
@@ -5,9 +5,7 @@ import { useState } from "react";
 
 function ActivityLogs({ tasks, projectId, logs }) {
   const [showModal, setShowModal] = useState(false);
-  const [totalLogs, setTotalLogs] = useState([logs]);
-
-  console.log(totalLogs);
+  const [totalLogs, setTotalLogs] = useState(logs);
 
   const handleOpenModal = () => {
     setShowModal(true);
@@ -16,15 +14,38 @@ function ActivityLogs({ tasks, projectId, logs }) {
   const handleCloseModal = () => {
     setShowModal(false);
   };
+
+  const logColors = {
+    100: "bg-blue-300",
+    200: "bg-yellow-300",
+    300: "bg-red-400",
+    400: "bg-orange-400",
+    500: "bg-gray-400",
+  };
+
   return (
     <div className="flex flex-col items-center space-y-4 rounded-md bg-white">
       <h1 className="text-4xl font-bold underline mb-4">Activity Log</h1>
       <div className="overflow-auto space-y-4 h-[30rem]">
-        <div className="w-72 h-24 bg-blue-300 rounded-md"></div>
-        <div className="w-72 h-24 bg-orange-400 rounded-md"></div>
-        <div className="w-72 h-24 bg-red-400 rounded-md"></div>
-        <div className="w-72 h-24 bg-yellow-300 rounded-md"></div>
-        <div className="w-72 h-24 bg-orange-400 rounded-md"></div>
+        {totalLogs.map((log) => {
+          const date = new Date(...log.timestamp);
+          const formattedDate = date.toLocaleDateString();
+          const formattedTime = date.toLocaleTimeString().slice(0, 5); 
+          const shortDescription = log.newDescription.length > 15
+          ? log.newDescription.slice(0, 15) + "(...)"
+          : log.newDescription;
+          return (
+            <div
+              key={log.id}
+              className={`w-72 h-24 rounded-md ${logColors[log.type]}`}
+            >
+              <p className="text-center">{shortDescription}</p>
+              <p className="text-center">{formattedDate}</p>
+              <p className="text-center">{formattedTime}</p>
+              <p className="text-center">{log.userName}</p>
+            </div>
+          );
+        })}
       </div>
       <Button onClick={handleOpenModal} className="mb-3">
         Create log
