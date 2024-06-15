40f010313b3285680f02eae12d0af3a71a350998
ricardoelias98 <uc2016225298@student.uc.pt>
2024-06-15 17:30:33 +0100

    pequenas edições de teste

--- projetofinal-frontend/src/Pages/GanttChartPage.jsx
+++ projetofinal-frontend/src/Pages/GanttChartPage.jsx
@@ -3,24 +3,29 @@ import { useParams } from "react-router-dom";
 import Layout from "../Components/Layout";
 import { Gantt, ViewMode } from "gantt-task-react";
 import "gantt-task-react/dist/index.css";
+import { useState } from "react";
 
 const GanttChartPage = () => {
   const { projectId } = useParams();
+  const [viewMode, setViewMode] = useState(ViewMode.Day);
 
   const tasks = [
     {
       start: new Date("2024-06-01"),
       end: new Date("2024-06-10"),
-      name: `Project ${projectId} Task 1`,
+      name: `Task 1`,
       id: "1",
       type: "task",
       progress: 45,
       dependencies: [],
+      styles: {
+        backgroundSelectedColor: "red",
+      },
     },
     {
       start: new Date("2024-06-11"),
       end: new Date("2024-06-20"),
-      name: `Project ${projectId} Task 2`,
+      name: `Task 2`,
       id: "2",
       type: "task",
       progress: 30,
@@ -31,8 +36,23 @@ const GanttChartPage = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Layout activeTab={0} activeSubTabProfile={0} />
-      <div className="gantt-container">
-        <Gantt tasks={tasks} viewMode={ViewMode.Day} />
+      <div className="gantt-outer-container w-full overflow-auto ml-10 mr-10">
+        <div className="gantt-container">
+          <select
+            value={viewMode}
+            onChange={(e) => setViewMode(e.target.value)}
+          >
+            <option value={ViewMode.Day}>Day</option>
+            <option value={ViewMode.Week}>Week</option>
+          </select>
+          <Gantt
+            tasks={tasks}
+            viewMode={viewMode}
+            preStepsCount={0}
+            rowHeight={25}
+            todayColor = "orange"
+          />
+        </div>
       </div>
     </div>
   );
