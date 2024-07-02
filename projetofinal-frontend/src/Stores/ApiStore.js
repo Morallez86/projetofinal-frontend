6ebaf4e352fc415a03d7fcd22cf49f0374ca30ed
ricardoelias98 <uc2016225298@student.uc.pt>
2024-07-02 16:49:36 +0100

    https

--- projetofinal-frontend/src/Stores/ApiStore.js
+++ projetofinal-frontend/src/Stores/ApiStore.js
@@ -1,9 +1,9 @@
 // src/Stores/apiStore.js
-import { create } from 'zustand';
+import { create } from "zustand";
 
 const useApiStore = create((set) => ({
-    apiUrl: 'http://localhost:8080/projetofinal-backend-1.0-SNAPSHOT/rest',
-    setApiUrl: (url) => set({ apiUrl: url }),
+  apiUrl: "https://localhost:8443/projetofinal-backend-1.0-SNAPSHOT/rest",
+  setApiUrl: (url) => set({ apiUrl: url }),
 }));
 
 export default useApiStore;
