04fee8a7174434657f1257720d26af7a203c2215
ricardoelias98 <uc2016225298@student.uc.pt>
2024-07-04 13:56:46 +0100

    footer consoante o idioma

--- projetofinal-frontend/src/components/Footer.jsx
+++ projetofinal-frontend/src/components/Footer.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import useUserStore from "../Stores/UserStore";
 
 function Footer() {
+  const languageApp = useUserStore((state) => state.language);
+  
+ 
+  const footerImage = languageApp === 'pt' 
+    ? require("../Assets/FooterPT.jpeg") 
+    : require("../Assets/FooterEN.jpeg") 
+  
   return (
-    <div className="w-full text-center relative bottom-0  z-50 mt-2 p-0">
+    <div className="w-full text-center relative bottom-0 z-50 mt-2 p-0">
       <img
-        src={require("../Assets/FooterEN.jpeg")}
+        src={footerImage}
         alt="Footer"
         className="w-full object-cover"
       />
