775f4c129397ab2a9c46a5c09e1210ea3dc6f4af
ricardoelias98 <uc2016225298@student.uc.pt>
2024-07-10 17:55:07 +0100

    sub tabs centradas

--- projetofinal-frontend/src/components/Layout.jsx
+++ projetofinal-frontend/src/components/Layout.jsx
@@ -269,7 +269,7 @@ function Layout({
               ></Tabs.Item>
             </Tabs>
 
-            <div className="w-full max-w-3xl">
+            <div className="flex flex-col items-center">
               {activeTab === 0 && (
                 <div>
                   <Tabs
